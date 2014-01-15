4f68a9542a1485221a14dba045bcb7d1b096bfec
Tompkins <1011047623@qq.com>
2014-01-15 10:14:16 +0800

    Add a file about HTTP POST

--- BaseHTTPServer/BaseHTTPServer_POST.py
+++ BaseHTTPServer/BaseHTTPServer_POST.py
@@ -18,3 +18,28 @@ class PostHandler(BaseHTTPRequestHandler):
         self.wfile.write('Client: %s\n' % str(self.client_address))
         self.wfile.write('User-agent: %s\n' %
                          str(self.headers['user-agent']))
+        self.wfile.write('Path: %s\n' % self.path)
+        self.wfile.write('Form data:\n')
+
+        # Echo back information about what was posted in the form
+        for field in form.keys():
+            field_item = form[field]
+            if field_item.filename:
+                # The field contains an uploaded file
+                file_data = field_item.file.read()
+                file_len = len(file_data)
+                del file_data
+                self.wfile.write(
+                    '\tUpload %s ad "%s" (%d bytes)\n' % \
+                        (field, field_item.filename, file_len))
+            else:
+                # Regular form value
+                slef.wfile.write('\t%s=%s\n' %
+                                 (field, form[field].value))
+        return
+    
+if __name__ == '__main__':
+    from BaseHTTPServer import HTTPServer
+    server = HTTPServer(('localhost', 8080), PostHandler)
+    print 'Starting server, use <Ctrl-C> to stop' 
+    server.serve_forever()
