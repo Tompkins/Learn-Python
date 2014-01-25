c7203a3c0271abef9dff56b13dc229263ceb53a3
Tompkins <1011047623@qq.com>
2014-01-25 10:55:49 +0800

    增加Animal的子类Prey和Predator

--- oop/oop.py
+++ oop/oop.py
@@ -57,3 +57,11 @@ class Animal(object):
 
     def __str__(self):
         return self.name
+
+class Prey(Animal):
+    def __init__(self, island, x = 0, y = 0, s = 'O'):
+        Animal.__init__(self, island, x, y, s)
+
+class Predator(Animal):
+    def __init__(self, island, x = 0, y = 0, s = 'X'):
+        Animal.__init__(self, island, x, y, s)
