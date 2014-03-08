6233d70f98e4b13d5a22936f33101d5f54d6dba0
Tompkins <1011047623@qq.com>
2014-03-08 20:02:05 +0800

    增加运算符重载方法

--- oop_py3/person.py
+++ oop_py3/person.py
@@ -7,7 +7,20 @@ class Person:
         self.job = job
         self.pay = pay
 
-bob = Person('Bob Smith')
-sue = Person("Sue Jones", job='dev', pay=10000)
-print(bob.name, bob.pay)
-print(sue.name, sue.pay)
+    def lastName(self):
+        return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
+    def __str__(self):
+        return '[Person: %s, %s]' % (self.name, self.pay)
+    
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=10000)
+    print(bob)
+    print(sue)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(.10)
+    print(sue)
