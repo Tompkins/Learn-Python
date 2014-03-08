8266baa14eaccbf4b1b379d9496078568edc3fe4
Tompkins <1011047623@qq.com>
2014-03-08 20:26:43 +0800

    重新定义Manager中的__init__的方法，从而提供mgr字符串。

--- oop_py3/person.py
+++ oop_py3/person.py
@@ -17,6 +17,9 @@ class Person:
         return '[Person: %s, %s]' % (self.name, self.pay)
 
 class Manager(Person):
+    def __init__(self, name, pay):
+        Person.__init__(self, name, 'mgr', pay)
+        
     def giveRaise(self, percent, bonus=0.1):
         Person.giveRaise(self, percent + bonus)     # good: argument original
     
@@ -28,11 +31,11 @@ if __name__ == '__main__':
     print(bob.lastName(), sue.lastName())
     sue.giveRaise(.10)
     print(sue)
-    tom = Manager('Tom Jones', 'mgr', 50000)
+    tom = Manager('Tom Jones', 50000)
     tom.giveRaise(0.1)
     print(tom.lastName())
     print(tom)
-    print('--All three--')
-    for object in (bob, sue, tom):
-        object.giveRaise(0.1)
-        print(object)
+    # print('--All three--')
+    # for object in (bob, sue, tom):
+    #     object.giveRaise(0.1)
+    #     print(object)
