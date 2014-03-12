3f11626d373cf67177934757cb084b0e8e6f5585
Tompkins <1011047623@qq.com>
2014-03-12 20:29:21 +0800

    修改了ListAdder的add方法，使用了列表支持的+运算符,更改了DictAdder的add方法。

--- oop_py3/adder.py
+++ oop_py3/adder.py
@@ -1,31 +1,23 @@
 class Adder:
-    def __init__(self, data):
+    def __init__(self, data = []):
         self.data = data
-    def add(self, y):
+    def add(self, other):
         print('Not implemented')
-    def __add__(self, y):
-        return self.add(y)
-
+    def __add__(self, other):
+        return self.add(other)
+    
 class ListAdder(Adder):
     def add(self, y):
-        for i in y:
-            self.data.append(i)
-        print(self.data)
-
+        return self.data + y
+    
 class DictAdder(Adder):
-    def add(self, y):
-        for keys in y:
-            if keys in self.data:
-                pass
-            else:
-                self.data[keys] = y[keys]
-        print(self.data)
+    def add(self, other):
+        new = {}
+        for k in self.data.keys(): new[k] = self.data[k]
+        for k in other.keys(): new[k] = other[k]
+        return new
         
-if __name__ == '__main__':
-    A = ListAdder([1, 2, 3])
-    A.add([4, 5, 6, 7, 8])
-    A + [9]
-    B = DictAdder({1:'2'})
-    B.add({1:'3', 2:'4'})
-    B + {3:'3'}
+
+    
+    
         
