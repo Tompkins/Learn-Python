0772abe05d8634f3fc3f3163f94a47f473459166
Tompkins <1011047623@qq.com>
2014-03-24 10:15:30 +0800

    定义一个ListInstace的混合类，定义了__str__方法，实现格式输出。

--- oop_py3/lister.py
+++ oop_py3/lister.py
@@ -1,2 +1,20 @@
 # -*- coding:utf-8 -*-
 # File lister.py
+
+class ListInstance:
+    """
+    Mix-in class that provides a formatted print() or str() of
+    instances via inheritance of __str__, coded here; displays
+    instance attrs only; self is the instanceof lowest class;
+    uses __X names to avoid clashing with client's attrs
+    """
+    def __str__(self):
+        return '<Instance of %s, address %s:\n%s>' % (
+                            self.__class__.__name__,        # My class's name
+                            id(self),                       # My address
+                            self.__attrnames())             # name=value list
+    def __attrnames(self):
+        result = ''
+        for attr in sorted(self.__dict__):                  # Instance attr dict
+            result += '\tname %s=%s\n' % (attr, self.__dict__[attr])
+        return result
