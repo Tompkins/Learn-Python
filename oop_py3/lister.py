34a4e1c0c9a7f864ee095f2ca3cee9f214dc9528
Tompkins <1011047623@qq.com>
2014-03-24 11:06:15 +0800

    编写ListInherited类，使用dir()函数列出所有的属性。

--- oop_py3/lister.py
+++ oop_py3/lister.py
@@ -18,3 +18,26 @@ class ListInstance:
         for attr in sorted(self.__dict__):                  # Instance attr dict
             result += '\tname %s=%s\n' % (attr, self.__dict__[attr])
         return result
+
+class ListInherited:
+    """
+    Use dir() to collect both instance attrs and names
+    inherited from its classes; Python 3 shows more
+    names than 2 because of the implied object superclass
+    in the new-style class model; getattr() fetches inherited
+    names not in self.__dict__; use __str__, not __repr__,
+    of else this loops when printing bound methods!
+    """
+    def __str__(self):
+        return '<Instance of %s, address %s:\n%s>' % (
+                            self.__class__.__name__,
+                            id(self),
+                            self.__attrnames())
+    def __attrnames(self):
+        result = ''
+        for attr in dir(self):                              # Instance dir()
+            if attr[:2] == '__' and attr[-2:] == '__':      # Skip internals
+                result += '\tname %s=<>\n' % attr
+            else:
+                result += '\tname %s=%s\n' % (attr, getattr(self, attr))
+        return result
