bb881d544515c6befa45ecdd59cba7208eabd5ae
Tompkins <1011047623@qq.com>
2014-03-24 10:35:42 +0800

    把ListInstance添加到一个类头部的超类列表中，子类在继承自有超类的同时，仍然可以获得__str__。

---
+++ oop_py3/testmixin.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+# File testmixin.py
+
+from lister import *                # Get lister tool classes
+
+class Super:
+    def __init__(self):             # Superclass.__init__
+        self.data1 = 'spam'         # Creat instance attrs
+    def ham(self):
+        pass
+
+class Sub(Super, ListInstance):     # Mix in ham and s __str__
+    def __init__(self):             # listers have access to self
+        Super.__init__(self)
+        self.data2 = 'eggs'         # More instance attrs
+        self.data3 = 42
+    def spam(self):                 # Define another method here
+        pass
+
+if __name__ == '__main__':
+    X = Sub()
+    print(X)                        # Run mixed-in __str__
