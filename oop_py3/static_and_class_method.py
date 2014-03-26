9534a78807f16221cb143f7018984c6ac5ba6ad8
Tompkins <1011047623@qq.com>
2014-03-26 10:49:59 +0800

    演示staticmethod和classmethod。

---
+++ oop_py3/static_and_class_method.py
@@ -0,0 +1,11 @@
+class Methods:
+    def imeth(self, x):         # Normal instance method: passed a self
+        print(self, x)
+    def smeth(x):               # Static: no instance passed
+        print(x)
+    def cmeth(cls, x):          # Class: gets class, not instance
+        print(cls, x)
+
+    smeth = staticmethod(smeth)     # Make smeth a static method
+    cmeth = classmethod(cmeth)      # Make cmeth a class method
+        
